b547398b1605f8056ccc497e011699d0cd2a2232
candydice <ishin.daniil@gmail.com>
2024-10-03 21:42:18 +0300

    add models with relationships

---
+++ app/Models/Event.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
