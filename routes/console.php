c45794fed537239cbc611d5e0f1e1198c4ac16ea
candydice <ishin.daniil@gmail.com>
2024-10-04 14:12:28 +0300

    feat command to delete logs

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('logs:clear', function() {
+    exec('rm -f ' . storage_path('logs/*.log'));
+    $this->comment('Logs have been removed!');
+})->describe('Remove log files');
