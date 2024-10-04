25486ff8a40cfb61364b907bd8b22846e8eac5c2
candydice <ishin.daniil@gmail.com>
2024-10-05 02:57:37 +0300

    change: event start_time format

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Carbon;
 
 class Event extends Model
 {
@@ -20,5 +22,12 @@ class Event extends Model
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
+    }
+
+    protected function startTime(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::parse($value)->format('d.m.Y H:i'),
+        );
     }
 }
