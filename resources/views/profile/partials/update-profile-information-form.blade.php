ed1054c6192abc6ee1c43b27226ef56e2a9c9753
candydice <ishin.daniil@gmail.com>
2024-10-06 03:00:03 +0300

    fix: localized notification of an unverified account

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -27,16 +27,16 @@
             @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
                 <div>
                     <p class="text-sm mt-2 text-gray-800">
-                        {{ __('Your email address is unverified.') }}
+                        Ваш аккаунт не подтвержден. Перейдите по ссылке, которая будет отправлена вам по электронной почте.
 
                         <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            {{ __('Click here to re-send the verification email.') }}
+                            Нажмите для отправки ссылки подтверждения.
                         </button>
                     </p>
 
                     @if (session('status') === 'verification-link-sent')
                         <p class="mt-2 font-medium text-sm text-green-600">
-                            {{ __('A new verification link has been sent to your email address.') }}
+                            Новая ссылка для подтверждения отправлена на вашу почту.
                         </p>
                     @endif
                 </div>
