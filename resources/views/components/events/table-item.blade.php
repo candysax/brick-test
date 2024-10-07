5eb63ba265551ea1ebd3a9d601e7efae001bd9ca
candydice <ishin.daniil@gmail.com>
2024-10-07 04:23:31 +0300

    change: changing the column width in the events table

--- resources/views/components/events/table-item.blade.php
+++ resources/views/components/events/table-item.blade.php
@@ -1,7 +1,7 @@
 @props(['event'])
 
 <tr class="bg-white border-b">
-    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+    <th scope="row" class="w-2/5 px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
         <a href="{{ route('events.show', [$event]) }}"><strong class="underline text-md">{{ $event->name }}</strong></a>
         @if(auth()->user()?->isAdmin())
             <div class="flex items-center space-x-2 mt-4">
@@ -11,18 +11,18 @@
             </div>
         @endif
     </th>
-    <td class="px-6 py-4">
+    <td class="w-1/5 px-6 py-4">
         {{ $event->formatedStartTime() }}
     </td>
-    <td class="px-6 py-4">
+    <td class="w-1/5 px-6 py-4">
         {{ $event->users->count() }} чел.
     </td>
-    <td class="px-6 py-4">
+    <td class="w-1/5 px-6 py-4">
         @foreach($event->categories as $category)
             {{ $category->name }}@if(! $loop->last),@endif
         @endforeach
     </td>
-    <td class="px-6 py-4">
+    <td class="w-1/5 px-6 py-4">
         <x-events.join-button :event="$event" />
     </td>
 </tr>
